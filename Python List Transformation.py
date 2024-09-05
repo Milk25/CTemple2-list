15f06381d6b06b5830d1d6b9c71cb8245c3e7648
milk19948 <97678058+Milk25@users.noreply.github.com>
2024-09-05 17:29:28 -0400

    Add files via upload

---
+++ Python List Transformation.py
@@ -0,0 +1,7 @@
+grades = [85,90,78,88,76,95,89,80,72,93]
+descending_order = grades
+descending_order = grades.sort(reverse = True)
+print(f"The Descending order is: {grades}")
+average_grade = sum(grades)
+calculate_average = average_grade / len(grades)
+print(calculate_average)
